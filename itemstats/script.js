c3d40bd53d5d91fd8bd0688773c32d117a811fdd
Defterdar <peter@pt-hosting.dk>
2023-12-25 16:15:59 +0100

    no sorting

--- itemstats/script.js
+++ itemstats/script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Function to display CSV data in the table
     function displayData(csvData, tableId) {
-        const dataArray = CSVToArray(csvData);
         const tableBody = document.getElementById(`${tableId}-body`);
+
+        if (!tableBody) {
+            console.error(`Error: Table body not found for ${tableId}`);
+            return;
+        }
+
+        const dataArray = CSVToArray(csvData);
 
         dataArray.forEach(row => {
             const newRow = tableBody.insertRow(tableBody.rows.length);
@@ -16,7 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Function to fetch and display CSV data for a specific table
     function fetchDataAndDisplay(tableId, csvFileName) {
         fetch(csvFileName)
-            .then(response => response.text())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data for ${tableId}. Status: ${response.status}`);
+                }
+                return response.text();
+            })
             .then(csvData => displayData(csvData, tableId))
             .catch(error => console.error(`Error fetching data for ${tableId}:`, error));
     }
@@ -38,25 +49,5 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         return dataArray;
-    }
-
-    // Function to sort the table by column
-    function sortTable(columnIndex, tableId) {
-        const table = document.getElementById(`${tableId}`);
-        const tbody = document.getElementById(`${tableId}-body`);
-        const rows = Array.from(tbody.rows);
-
-        rows.sort((a, b) => {
-            const cellA = a.cells[columnIndex].textContent;
-            const cellB = b.cells[columnIndex].textContent;
-
-            return isNaN(cellA) ? cellA.localeCompare(cellB) : cellA - cellB;
-        });
-
-        while (tbody.firstChild) {
-            tbody.removeChild(tbody.firstChild);
-        }
-
-        rows.forEach(row => tbody.appendChild(row));
     }
 });
